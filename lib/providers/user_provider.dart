157f6eb4d51bba0d2bdfbf8245d7d353a96650cb
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-02-06 22:21:22 -0300

    feat: create UserProvider

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,41 @@
+import 'dart:collection';
+
+import 'package:e_commerce_app/apis/e_commerce_api.dart';
+import 'package:e_commerce_app/models/user.dart';
+import 'package:flutter/cupertino.dart';
+
+class UserProvider with ChangeNotifier {
+  User? _currentUser;
+  String? accessToken = '';
+  bool logged = false;
+
+  final ApiIN8 _api = ApiIN8();
+
+  bool isUserLoggedIn() {
+    if (accessToken != '' && logged) return true;
+    return false;
+  }
+
+  Future<bool> logIn(String email, String password) async {
+    var result = await _api.login(email, password);
+    if (result.accessToken.length > 5) {
+      logged = true;
+      accessToken = result.accessToken;
+      _currentUser = User(email: email, name: result.name);
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> signUp(String name, String email, String password) async {
+    var userInfo = User(email: email, name: name);
+    var result = await _api.signUp(userInfo, password);
+    if (result.accessToken.length > 5) {
+      logged = true;
+      accessToken = result.accessToken;
+      _currentUser = User(email: email, name: result.name);
+      return true;
+    }
+    return false;
+  }
+}
